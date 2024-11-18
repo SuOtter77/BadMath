9ca1aaf5e2355706e314c92d2204f4b8f1f27eb0
AG <186358781+SuOtter77@users.noreply.github.com>
2024-11-18 00:30:58 -0500

    Fixed two variable bugs. Increased precision and removed float marker.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,21 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
+float average(int i1, int i2, int i3) {
+	return (i1 + i2 + i3) / 3.0; // 'f' is unneeded
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1 = 0; int n2 = 0; int n3 = 0; //declared these as integers
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3; // changed n2 to n3
 
-	float a = average(n1, n2, n3);
+	float avg = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(3) << fixed << "The average is " << avg << endl; // added better precision
 
 	return 0;
 }
